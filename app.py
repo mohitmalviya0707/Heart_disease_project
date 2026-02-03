42bc03dc3a20503f959207cff872454e643f3acc
mohitmalviya0707 <mohitmalviya0107@gmail.com>
2026-02-03 19:06:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ model = joblib.load("KNN_heart.pkl")
 scaler = joblib.load("scaler.pkl")
 expected_columns = joblib.load("columns.pkl")
 
-st.title("Heart Stroke Prediction by akarsh")
+st.title("Heart Stroke Prediction by Mohit")
 st.markdown("Provide the following details to check your heart stroke risk:")
 
 # Collect user input
